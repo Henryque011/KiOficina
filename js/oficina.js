c47568ca393044ab98faf7ef617e121c0ac13981
Henryque Nonato Silva <132319412+Henryque011@users.noreply.github.com>
2024-10-14 20:57:37 -0300

    Js
    JavaScript

---
+++ js/oficina.js
@@ -0,0 +1,20 @@
+// [;n]     (*)
+//  ____   ___       
+//  |   \  | | (*)  
+//  | |\ \ | |     
+//  | | \ \| |  (*) 
+//  |_|  )___| 
+//   
+//          (*)
+
+
+
+
+// BANNER - SLIDE
+$('.slide').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    autoplaySpeed: 2000,
+}); 
